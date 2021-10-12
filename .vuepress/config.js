9e511d98e25b223a83f3674c6a2d9f32e413d5fd
liuzhiyan5 <1172258408@qq.com>
2021-10-12 20:02:30 +0800

    大概框架完成

---
+++ .vuepress/config.js
@@ -0,0 +1,141 @@
+const sidebar = require("./sidebar.js");
+module.exports = {
+  // 网站标题
+  title: "刘謀的博客",
+  description: "莫愁前路无知己，天下谁人不识君！",
+  dest: "dist", // 打包路径
+  base: "/blog/",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "/favicon.ico",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
+  ],
+  plugins: [
+    ["vuepress-plugin-code-copy", true],
+    ["@vuepress/medium-zoom"],
+    [
+      "@vuepress/pwa", // 更新刷新插件
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "发现新内容可用",
+          buttonText: "刷新",
+        },
+      },
+    ],
+    // 复制弹窗
+    [
+      "vuepress-plugin-nuggets-style-copy",
+      {
+        copyText: "复制代码",
+        tip: {
+          content: "复制成功!",
+        },
+      },
+    ],
+  ],
+  theme: "reco",
+  themeConfig: {
+    // 黑暗模式
+    mode: "light",
+    // 顶部导航栏
+    nav: [
+      {
+        text: "主页",
+        link: "/",
+        icon: "reco-home",
+      },
+      {
+        text: "时间线",
+        link: "/timeline/",
+        icon: "reco-date",
+      },
+      {
+        text: "文档",
+        icon: "reco-message",
+        items: [
+          // {
+          //   text: "前端",
+          //   link: "/docs/Front-end/",
+          // },
+        ],
+      },
+      {
+        text: "关于我",
+        icon: "reco-message",
+        items: [
+          {
+            text: "GitHub",
+            link: "https://github.com/liuzhiyan5",
+            icon: "reco-github",
+          },
+        ],
+      },
+    ],
+    // 侧边栏
+    sidebar: sidebar,
+    type: "blog",
+    blogConfig: {
+      category: {
+        location: 2,
+        text: "分类",
+      },
+      tag: {
+        location: 3,
+        text: "标签",
+      },
+    },
+    // 框架作者，友情链接
+    friendLink: [
+      {
+        title: "午后南杂",
+        desc: "Enjoy when you can, and endure when you must.",
+        email: "1156743527@qq.com",
+        link: "https://www.recoluan.com",
+      },
+      {
+        title: "vuepress-theme-reco",
+        desc: "A simple and beautiful vuepress Blog & Doc theme.",
+        avatar:
+          "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        link: "https://vuepress-theme-reco.recoluan.com",
+      },
+      {
+        title: "Silence-dream",
+        desc: "白月初",
+        avatar: "https://avatars.githubusercontent.com/u/42824008?v=4",
+        link: "https://github.com/Silence-dream",
+      },
+    ],
+    huawei: true,
+    logo: "/logo.png",
+    search: true,
+    searchMaxSuggestions: 10,
+    lastUpdated: "Last Updated",
+    author: "謀.LIU",
+    authorAvatar: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201701%2F24%2F20170124004459_aCT53.thumb.700_0.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1636600371&t=a64fc8b9c2de14ff7d8e8383fb4979a1",
+    record: "xxxx",
+    startYear: "2017",
+    // 备案
+    record: "ICP 备案文案",
+    recordLink: "ICP 备案指向链接",
+    cyberSecurityRecord: "公安部",
+    cyberSecurityLink:
+      "https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh",
+    subSidebar: "auto", //在所有页面中启用自动生成子侧边栏，原 sidebar 仍然兼容
+  },
+  markdown: {
+    lineNumbers: true,
+  },
+};
---
+++ README.md
@@ -0,0 +1,31 @@
+---
+home: true
+heroText: 博客
+tagline: 莫愁前路无知己，天下谁人不识君
+# 首页按钮
+actionText: 巅峰之路
+actionLink: /docs/Front-end/Vue/
+lang: zh-cn
+# 首页图
+# heroImage: /hero.png
+# heroImageStyle: {
+#   maxWidth: '600px',
+#   width: '100%',
+#   display: block,
+#   margin: '9rem auto 2rem',
+#   background: '#fff',
+#   borderRadius: '1rem',
+# }
+bgImageStyle: { height: "450px" }
+# isShowTitleInHome: false
+# footer: 11111111
+features:
+  - title: Yesterday
+    details: 开发一款看着开心、写着顺手的 vuepress 博客主题
+  - title: Today
+    details: 希望帮助更多的人花更多的时间在内容创作上，而不是博客搭建上
+  - title: Tomorrow
+    details: 希望更多的爱好者能够参与进来，帮助这个主题更好的成长
+---
+
+本博客所有文章除特别声明外，均采用 [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh) 许可协议
